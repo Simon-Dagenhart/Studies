3fb82154de765da917c3a40ae19adb92b373d625
Simon-Dagenhart <simonkazage007@gmail.com>
2026-07-13 05:57:21 +0300

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var navToggle = document.getElementById('navToggle');
-    var navList = document.getElementById('navList');
-    if (navToggle && navList) {
-        navToggle.addEventListener('click', function () {
-            navList.classList.toggle('open');
-        });
-    }
-});
